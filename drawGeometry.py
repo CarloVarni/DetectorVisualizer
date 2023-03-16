ef08ae60665cacfe174a5347def55ef2e54967b2
Carlo Varni <carlovarni@pccvarni.dyndns.cern.ch>
2023-03-16 14:19:59 +0100

    corrected eta lines

--- drawGeometry.py
+++ drawGeometry.py
@@ -22,26 +22,8 @@ if __name__ == "__main__":
     grid = Grid(pixelGridBoundaries)
     pixels = ITkPixelDetector(rangeR, rangeZ, rangeInclusive) #
     strips = ITkStripDetector(rangeR, rangeZ, rangeInclusive) #
-    
-    # Get The seeds
-    grs = []
-    seeds = []
-    with open('seeds.txt', 'r') as inFile:
-        for line in inFile:
-            [b_z, b_r, m_z, m_r, t_z, t_r] = re.findall('seed: \[(.*), (.*)\] \[(.*), (.*)\] \[(.*), (.*)\]', line)[0]            
-            gr = TGraph()
-            gr.SetPoint(0, float(b_z), float(b_r))
-            gr.SetPoint(1, float(m_z), float(m_r))
-            gr.SetPoint(2, float(t_z), float(t_r))
-            gr.SetMarkerColor(2)
-            gr.SetMarkerStyle(20)
-            gr.SetLineColor(2)
-            gr.SetMarkerSize(0.4)
-            grs.append(gr)
-
-    grs = grs[:4]
-            
-    canvas = TCanvas()
+                
+    canvas = TCanvas("canvas", "canvas")
     canvas.SetTicks()
     pixels.draw(canvas)
     strips.draw(canvas, "SAME")
@@ -50,21 +32,19 @@ if __name__ == "__main__":
     line_eta = TLine()
     line_eta.SetLineStyle(3)
     line_eta.SetLineColorAlpha(4, 1)
-    # eta = 1
-    line_eta.DrawLine( 0, 0, rangeR.maximum()/math.tan(0.704774), rangeR.maximum() )
-    line_eta.DrawLine( 0, 0, -rangeR.maximum()/math.tan(0.704774), rangeR.maximum() )
-    # eta = 2
-    line_eta.DrawLine( 0, 0, rangeR.maximum()/math.tan(0.268779), rangeR.maximum() )
-    line_eta.DrawLine( 0, 0, -rangeR.maximum()/math.tan(0.268779), rangeR.maximum() )
-    # eta = 3
-    line_eta.DrawLine( 0, 0, rangeZ.maximum(), rangeZ.maximum() * math.tan(0.0987791))
-    line_eta.DrawLine( 0, 0, -rangeZ.maximum(), rangeZ.maximum() * math.tan(0.0987791))
-    # eta = 4
-    line_eta.DrawLine( 0, 0, rangeZ.maximum(), rangeZ.maximum() * math.tan(0.0357791))
-    line_eta.DrawLine( 0, 0, -rangeZ.maximum(), rangeZ.maximum() * math.tan(0.0357791))
 
-    for gr in grs:
-        gr.Draw("PLSAME")
+    # [eta 1, eta 2, eta 3, eta 4]
+    for theta in [0.704774, 0.268779, 0.0987791, 0.0357791]:        
+        y_value_atLimitZ = math.tan(theta) * rangeZ.maximum()
+        x_value_atLimitR = rangeR.maximum()/math.tan(theta)
+
+        if x_value_atLimitR < rangeZ.maximum():
+            line_eta.DrawLine( 0, 0, x_value_atLimitR, rangeR.maximum() )
+            line_eta.DrawLine( 0, 0, -x_value_atLimitR, rangeR.maximum() )
+        else:
+            line_eta.DrawLine( 0, 0, rangeZ.maximum(), y_value_atLimitZ)
+            line_eta.DrawLine( 0, 0, -rangeZ.maximum(), y_value_atLimitZ)
+        
     grid.draw(canvas, rangeR)
     canvas.Draw()
     canvas.SaveAs("PixelDetector.pdf")
